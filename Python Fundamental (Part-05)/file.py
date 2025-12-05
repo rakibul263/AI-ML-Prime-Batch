89c5a11e901f9591e2213f9f9874baaf4222ae61
rakibul263 <rakibulhasanshuvo206@gmail.com>
2025-12-05 18:25:34 +0600

    with operation

--- Python Fundamental (Part-05)/file.py
+++ Python Fundamental (Part-05)/file.py
@@ -1,7 +1,7 @@
 f = open("sample.txt", "r");
-# data = f.read();
-data = f.readline();
+data = f.read();
+# data = f.readline();
 print(data);
 
-f = open("sample.txt", "w");
+f = open("sample.txt", "a");
 f.write("Daffodil International University");
